f2d67ef7e498706c1fbcd2676c2b6a14baa826f8
maedomarimanato <138412497+maedomarimanato@users.noreply.github.com>
2026-02-20 15:11:15 +0900

    Helpコメント追加

--- GameJam2026/Scenes/HelpScene/HelpScene.cpp
+++ GameJam2026/Scenes/HelpScene/HelpScene.cpp
@@ -7,12 +7,13 @@ eSceneType HelpScene::Update()
 {
     InputManager* input = InputManager::GetInstance();
 
-    // Bボタンでタイトルに戻る
+    // Bボタンが押されたらタイトルへ戻る
     if (input->GetButtonDown(PAD_B))
     {
         return eSceneType::E_TITLE;
     }
 
+    // 何もなければ現在のシーンを維持
     return GetNowScene();
 }
 
@@ -20,6 +21,7 @@ void HelpScene::Draw() const
 {
     SetFontSize(30);
 
+    // ヘルプ画面の説明テキストを表示
     DrawString(200, 150, "=== HELP ===", 0xffffff);
     DrawString(200, 200, "Aボタン : ゲーム開始", 0xffffff);
     DrawString(200, 240, "Bボタン : 戻る", 0xffffff);
--- GameJam2026/Scenes/HelpScene/HelpScene.h
+++ GameJam2026/Scenes/HelpScene/HelpScene.h
@@ -7,12 +7,13 @@ public:
     HelpScene() {}
     ~HelpScene() {}
 
-    void Initialize() override {}
-    void Finalize() override {}
+    void Initialize() override {}   // ヘルプ画面は特に初期化なし
+    void Finalize() override {}     // 終了処理も特になし
 
-    eSceneType Update() override;
-    void Draw() const override;
+    eSceneType Update() override;   // 入力受付（戻るボタン）
+    void Draw() const override;     // ヘルプ内容の描画
 
+    // このシーンが HELP であることを返す
     eSceneType GetNowScene() const override
     {
         return eSceneType::E_HELP;
