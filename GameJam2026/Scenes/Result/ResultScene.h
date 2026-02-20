18e7c5c9d634de103510265795dd73840aa68b6b
maedomarimanato <138412497+maedomarimanato@users.noreply.github.com>
2026-02-20 09:06:09 +0900

    ResorceManager追加
    RsourceManagetの追加とPlayerの軽い修正

--- GameJam2026/Scenes/Result/ResultScene.h
+++ GameJam2026/Scenes/Result/ResultScene.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "DxLib.h"
+
+class ResultScene
+{
+public:
+    ResultScene();
+
+    void Initialize(int score, int correct);
+    void Update();
+    void Draw() const;
+    void Finalize();
+
+private:
+    int background_;
+    int bgm_;
+    int se_;
+
+    int score_;
+    int correct_;
+};
